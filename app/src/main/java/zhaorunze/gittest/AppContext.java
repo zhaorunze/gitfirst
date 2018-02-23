a84f81df240d2df6f59f0459b2749823dd88f836
runzezhao <1159963642@qq.com>
2018-02-23 14:43:59 +0800

    hello

---
+++ app/src/main/java/zhaorunze/gittest/AppContext.java
@@ -0,0 +1,12 @@
+package zhaorunze.gittest;
+
+import android.app.Application;
+
+/**
+ * Created by zhaorunze on
+ * 2018/2/23 14:37
+ * E-Mail Address：1159963642@qq.com
+ */
+
+public class AppContext extends Application {
+}
--- app/src/main/java/zhaorunze/gittest/ui/activity/MainActivity.java
+++ app/src/main/java/zhaorunze/gittest/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package zhaorunze.gittest;
+package zhaorunze.gittest.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import zhaorunze.gittest.R;
 
 public class MainActivity extends AppCompatActivity {
 
